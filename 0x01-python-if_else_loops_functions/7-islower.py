ce6f8e153568fb10671ef4070c0e45d1def2a9e1
Obedy Daka <obedydaka01@gmail.com>
2023-08-08 15:00:56 -0700

    First commit

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def islower(c):
+    chd = ord(c)
+    if chd >= 97 and chd <= 122:
+        return True
+    else:
+        return False
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+This directory is about the if else conditions in Python
