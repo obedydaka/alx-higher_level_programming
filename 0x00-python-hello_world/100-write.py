746dedca2e505bec17da0ba058bf18b630ab7185
Obedy Daka <obedydaka01@gmail.com>
2023-08-14 14:01:31 -0700

    Last push

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import sys
+
+def main():
+    msg = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+    sys.stderr.write(msg + '\n')
+    sys.exit(1)
+
+if __name__ == "__main__":
+    main()
