1c8da5d5a98abc6cf5733475ab1ccceef3bea629
Obedy Daka <obedydaka01@gmail.com>
2023-08-20 09:56:11 -0700

    Final push

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,11 +1,5 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
 import sys
 
-def main():
-    msg = "and that piece of art is useful - Dora Korpar, 2015-10-19"
-    sys.stderr.write(msg + '\n')
-    sys.exit(1)
-
-if __name__ == "__main__":
-    main()
+sys.stderr.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n')
+exit(1)
