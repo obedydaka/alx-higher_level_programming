8c49fe74006f95adfeeec1b992acace1cdcb5ba4
Obedy Daka <obedydaka01@gmail.com>
2023-09-07 12:22:23 -0700

    First phase

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri September 7 2023 20:08:02
+
+@author: Obedy Daka
+"""
+
+class Rectangle:
+    """
+    An empty class Rectangle that defines a rectangle figure
+
+    Attributes:
+        empty
+    """
+
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,2 @@
+Ths directory is the extension of tasks on python classes and objects
+
